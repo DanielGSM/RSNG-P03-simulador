80c56ff677f01dfed993a6a1482c5724c19ce165
DanielGSM <danigarciasan@hotmail.com>
2016-05-16 18:23:52 +0200

    Reading arguments

--- Simulator/src/simulator/Simulator.java
+++ Simulator/src/simulator/Simulator.java
@@ -17,12 +17,21 @@ public class Simulator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //we read the arguments
+        int numThreads = Integer.parseInt(args[0]);
+        int queueSize = Integer.parseInt(args[1]);
+        System.out.println("Starting the simulator with " + numThreads
+                + " threads and a queue of " + queueSize + " clients\n\n\n");
+
+        //the events reader is prepared
         EventsReader eventsReader = new EventsReader(
                 File.separator + "opt3"
                 + File.separator + "rsng"
                 + File.separator + "rsng06"
                 + File.separator + "Desktop"
                 + File.separator + "entradas.txt");
+
+        //we read the next events while there are still some left
         try {
             ArrivalEvent lastArrivalEvent = eventsReader.nextArrivalEvent();
             while (lastArrivalEvent != null) {
